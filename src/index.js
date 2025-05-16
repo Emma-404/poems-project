9f2cb2195f55f1fa689e985b8ca8d70d75446ae1
Emma-404 <emmamboronad@gmail.com>
2025-05-16 10:10:15 +0200

    Fixed Js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function displayPoem(response) {
   console.log("poem generated");
   new Typewriter("#poem", {
-    strings: response.date.answer,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
   });
@@ -15,10 +15,9 @@ function generatePoem(event) {
   let prompt = `Please generate a short French poem about ${instructionsInput.value}`;
   let context =
     "You are very knowledgable and romantic poem expert and you like to write poems, could you please generate a short poem in French, please provide your reply in format HTML and separate each line with a <br>. Please make sure to take in consideration user instructions when generatin and sign the poem at the end with 'SheCodes AI'. Do not include a title to the poem";
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={apiKey}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt={Please generate a short French poem about ${instructionsInput.value}}&context={You are very knowledgable and romantic poem expert and you like to write poems, could you please generate a short poem in French, please provide your reply in format HTML and separate each line with a <br>. Please make sure to take in consideration user instructions when generatin and sign the poem at the end with 'SheCodes AI'. Do not include a title to the poem}&key={4e369354et3395e2b9c0oec6dfc1eab9}`;
 
   console.log("Generating poem");
-
   axios.get(apiURL).then(displayPoem);
 }
 
