0fc1ecf45c34da363d401eab081a834232ddf9f9
Emma-404 <emmamboronad@gmail.com>
2025-05-16 11:55:06 +0200

    Added some UI
    html,js and css
    +animation

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     "You are very knowledgable and romantic poem expert and you like to write poems, could you please generate a short poem in French, please provide your reply in format HTML and separate each line with a <br>. Please make sure to take in consideration user instructions when generatin and sign the poem at the end with 'SheCodes AI'. Do not include a title to the poem";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=Please generate a short French poem about ${instructionsInput.value}&context=You are very knowledgable and romantic poem expert and you like to write poems, could you please generate a short poem in French, please provide your reply in format HTML and separate each line with a <br>. Please make sure to take in consideration user instructions when generatin and sign the poem at the end with 'SheCodes AI'. Do not include a title to the poem&key=4e369354et3395e2b9c0oec6dfc1eab9`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating poem about ${instructionsInput.value}, please hang on</div>`;
+
   console.log("Generating poem");
   axios.get(apiURL).then(displayPoem);
 }
